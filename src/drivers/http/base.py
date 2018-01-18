649547f7ad9b97d98fbe39c9a6fd54e430f9f464
kshibkov <kshibkov@fix.ru>
2018-01-18 18:13:45 +0300

    dont add format when alter

--- src/drivers/http/base.py
+++ src/drivers/http/base.py
@@ -7,11 +7,18 @@ from . import connector
 VERSION = (0, 0, 2, None)
 
 
+def add_format(sql):
+    dont_add = ['alter', 'drop', 'create']
+    if not any([sql[:len(x)].lower() == x for x in dont_add]):
+        return sql + ' FORMAT TabSeparatedWithNamesAndTypes'
+    return sql
+
+
 class ClickHouseExecutionContext(ClickHouseExecutionContextBase):
     def pre_exec(self):
         # TODO: refactor
         if not self.isinsert and not self.isddl:
-            self.statement += ' FORMAT TabSeparatedWithNamesAndTypes'
+            self.statement = add_format(self.statement)
 
 
 class ClickHouseDialect_http(ClickHouseDialect):
@@ -35,8 +42,7 @@ class ClickHouseDialect_http(ClickHouseDialect):
         return (db_url, db_name, url.username, url.password), kwargs
 
     def _execute(self, connection, sql):
-        sql += ' FORMAT TabSeparatedWithNamesAndTypes'
-        return connection.execute(sql)
+        return connection.execute(add_format(sql))
 
 
 dialect = ClickHouseDialect_http
