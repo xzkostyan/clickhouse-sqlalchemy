30712d94f94438428ebaca5c86edad306323bf5f
const <const@email.com>
2018-11-12 17:30:59 +0300

    up version 0.0.3.4

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ dialects = [
 
 setup(
     name='clickhouse-sqlalchemy',
-    version='0.0.3.3',
+    version='0.0.3.4',
 
     description='Simple ClickHouse SQLAlchemy Dialect',
     long_description=long_description,
