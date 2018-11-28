48589f685bf6371633c7d2726e5991102553f505
Nikita Kozlovsky <nikitka@gmail.com>
2018-11-28 20:08:31 +0300

    add Numeric type support (minimum 18.12.13 required)

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,8 +1,9 @@
 from datetime import datetime
-
-from sqlalchemy import Column
+from decimal import Decimal
+from sqlalchemy import Column, Numeric
 from sqlalchemy.sql.ddl import CreateTable
 from clickhouse_sqlalchemy import types, engines, Table
+from clickhouse_sqlalchemy.exceptions import DatabaseException
 
 from tests.testcase import TypesTestCase
 
@@ -26,3 +27,41 @@ class DateTimeTypeTestCase(TypesTestCase):
             self.compile(CreateTable(self.table)),
             'CREATE TABLE test (x DateTime) ENGINE = Memory'
         )
+
+
+class NumericTypeTestCase(TypesTestCase):
+    table = Table(
+        'test', TypesTestCase.metadata(),
+        Column('x', Numeric(10, 2)),
+        engines.Memory()
+    )
+
+    def test_create_table(self):
+        self.assertEqual(
+            self.compile(CreateTable(self.table)),
+            'CREATE TABLE test (x Decimal(10,2)) ENGINE = Memory'
+        )
+
+    def test_select_insert(self):
+        x = Decimal('123456789.12')
+
+        with self.create_table(self.table):
+            self.session.execute(self.table.insert(), [{'x': x}])
+            self.assertEqual(self.session.query(self.table.c.x).scalar(), x)
+
+    def test_insert_truncate(self):
+        value = Decimal('123.129999')
+        expected = Decimal('123.12')
+
+        with self.create_table(self.table):
+            self.session.execute(self.table.insert(), [{'x': value}])
+            self.assertEqual(self.session.query(self.table.c.x).scalar(),
+                             expected)
+
+    def test_insert_overflow(self):
+        value = Decimal('12345678901234567890.1234567890')
+
+        with self.create_table(self.table):
+            with self.assertRaisesRegex(DatabaseException,
+                                        'Column x: argument out of range$'):
+                self.session.execute(self.table.insert(), [{'x': value}])
