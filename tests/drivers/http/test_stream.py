6f7b0f4f15ca404a03df72cdf940244ce8cf2298
Anton Vasilyev <hhell@yandex-team.ru>
2020-01-17 15:48:23 +0300

    linting

--- tests/drivers/http/test_stream.py
+++ tests/drivers/http/test_stream.py
@@ -1,13 +1,10 @@
-from sqlalchemy import Column
 
-from clickhouse_sqlalchemy import types, Table
 from tests.testcase import HttpSessionTestCase
-from tests.session import http_stream_session, http_session, native_session
+from tests.session import http_stream_session
 
 
 class StreamingHttpTestCase(HttpSessionTestCase):
-    # session = http_stream_session
-    session = native_session
+    session = http_stream_session
 
     def test_streaming(self):
         power = 4
@@ -16,7 +13,9 @@ class StreamingHttpTestCase(HttpSessionTestCase):
         # est. 1 second on 10**5 lines for TSV,
         # 2.5 seconds on uncythonized native protocol.
         query = 'select ' + ', '.join(
-            "arrayJoin(['{0}', '2', '3', '4', '5', '6', '7', '8', '9', '10']) as a{0}".format(num)
+            ("arrayJoin(["
+             "'{0}', '2', '3', '4', '5', "
+             "'6', '7', '8', '9', '10']) as a{0}").format(num)
             for num in range(power))
         res = self.session.execute(query)
         count = sum(1 for _ in res)
