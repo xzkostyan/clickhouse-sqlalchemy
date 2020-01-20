eaa5f1f10838a6c6342bfb4df63c60fdf3716e75
Anton Vasilyev <hhell@yandex-team.ru>
2020-01-20 18:17:04 +0300

    Test the fetchmany and fetchall

--- tests/drivers/http/test_stream.py
+++ tests/drivers/http/test_stream.py
@@ -4,10 +4,13 @@ from tests.session import http_stream_session
 
 
 class StreamingHttpTestCase(HttpSessionTestCase):
-    session = http_stream_session
 
-    def test_streaming(self):
-        power = 4
+    session = http_stream_session
+    power = 4
+
+    def make_query(self, power=None):
+        if power is None:
+            power = self.power
         # `10**power` rows.
         # Timing order of magnitude:
         # est. 1 second on 10**5 lines for TSV,
@@ -17,6 +20,41 @@ class StreamingHttpTestCase(HttpSessionTestCase):
              "'{0}', '2', '3', '4', '5', "
              "'6', '7', '8', '9', '10']) as a{0}").format(num)
             for num in range(power))
+        return query
+
+    def test_streaming(self):
+        power = self.power
+        query = self.make_query(power=power)
         res = self.session.execute(query)
         count = sum(1 for _ in res)
-        assert count == 10 ** power
+        self.assertEqual(count, 10 ** power)
+
+    def test_fetchmany(self):
+        power = self.power - 1
+        query = self.make_query(power=power)
+        res = self.session.execute(query)
+
+        count = 0
+        while True:
+            block = res.fetchmany(1000)
+            if not block:
+                break
+            count += sum(1 for _ in block)
+
+        self.assertEqual(count, 10 ** power)
+
+    def test_fetchall(self):
+        power = self.power - 2
+        assert power >= 1
+        query = self.make_query(power=power)
+        res = self.session.execute(query)
+
+        count = 0
+
+        block = res.fetchmany(7)
+        count += sum(1 for _ in block)
+
+        block = res.fetchall()
+        count += sum(1 for _ in block)
+
+        self.assertEqual(count, 10 ** power)
