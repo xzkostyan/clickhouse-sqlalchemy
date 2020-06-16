57130d8d4b503bba6bd77c8d9dc109a70a76d13c
ods <denis.otkidach@gmail.com>
2020-06-16 12:22:41 +0300

    Test for LIMIT BY with offset

--- tests/sql/test_selectable.py
+++ tests/sql/test_selectable.py
@@ -71,6 +71,21 @@ class SelectTestCase(BaseTestCase):
             'SELECT t1.x FROM t1 ORDER BY t1.x LIMIT 1 BY t1.x'
         )
 
+    def test_limit_by_with_offset(self):
+        table = self._make_table()
+
+        query = select([table.c.x]).order_by(table.c.x)\
+            .limit_by(table.c.x, offset=1, limit=2)
+        self.assertEqual(
+            self.compile(query),
+            'SELECT t1.x FROM t1 ORDER BY t1.x '
+            'LIMIT %(param_1)s, %(param_2)s BY t1.x'
+        )
+        self.assertEqual(
+            self.compile(query, literal_binds=True),
+            'SELECT t1.x FROM t1 ORDER BY t1.x LIMIT 1, 2 BY t1.x'
+        )
+
     def test_nested_type(self):
         table = self._make_table(
             't1',
