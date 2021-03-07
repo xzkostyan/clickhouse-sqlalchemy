564709cf02d8b162333f2a39c3cbf8dbf778a4fc
Konstantin Lebedev <kostyan.lebedev@gmail.com>
2021-03-07 17:28:59 +0300

    Fix test_utils tests

--- tests/drivers/http/test_utils.py
+++ tests/drivers/http/test_utils.py
@@ -1,4 +1,7 @@
+# coding: utf-8
+
 from clickhouse_sqlalchemy.drivers.http.utils import unescape, parse_tsv
+from clickhouse_sqlalchemy.util import compat
 from tests.testcase import BaseTestCase
 
 
@@ -9,12 +12,18 @@ class HttpUtilsTestCase(BaseTestCase):
         self.assertListEqual(actual, [u'', u'a', u'\ufffd'])
 
     def test_unescape_surrogates(self):
+        if compat.PY2:
+            self.skipTest('surrogateescape is not supported in Python 2')
+
         test_values = [b'', b'a', b'\xc3\xa3\xff\xf6\0\x00\n', b'a\n\\0']
         actual = [unescape(t, errors='surrogateescape') for t in test_values]
         expected = [u'', u'a', u'ã\udcff\udcf6\x00\x00\n', u'a\n\x00']
         self.assertListEqual(actual, expected)
 
     def test_reverse_surrogates(self):
+        if compat.PY2:
+            self.skipTest('surrogateescape is not supported in Python 2')
+
         # What's stored in the database:
         expected = [b'', b'a', b'\xc3\xa3\xff\x55\xf6\0\x00\x45\0\n']
 
