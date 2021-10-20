ebb20ab955d2dffb3e4f7fedc2486fb8c914d33a
Konstantin Lebedev <kostyan.lebedev@gmail.com>
2021-10-20 22:02:53 +0300

    Fix #144

--- tests/sql/test_is_distinct_from.py
+++ tests/sql/test_is_distinct_from.py
@@ -1,31 +1,35 @@
+# coding: utf-8
+
+from parameterized import parameterized
 from sqlalchemy import select, literal
 
 from tests.testcase import BaseTestCase
 
 
 class IsDistinctFromTestCase(BaseTestCase):
-
-    def _select_bool(self, expr) -> bool:
+    def _select_bool(self, expr):
         query = select([expr])
         (result,), = self.session.execute(query).fetchall()
         return bool(result)
 
-    def test_is_distinct_from(self):
-        distinct_value_pairs = [
-            (1, 2),
-            (1, None),
-            (None, "NULL"),
-            (None, "ᴺᵁᴸᴸ"),
-            ((1, None), (2, None)),
-            ((1, (1, None)), (1, (2, None)))
-        ]
-        for a, b in distinct_value_pairs:
-            with self.subTest():
-                self.assertTrue(self._select_bool(literal(a).is_distinct_from(b)))
-                self.assertFalse(self._select_bool(literal(a).isnot_distinct_from(b)))
+    @parameterized.expand([
+        (1, 2),
+        (1, None),
+        (None, "NULL"),
+        (None, u"ᴺᵁᴸᴸ"),
+        ((1, None), (2, None)),
+        ((1, (1, None)), (1, (2, None)))
+    ])
+    def test_is_distinct_from(self, a, b):
+        self.assertTrue(self._select_bool(literal(a).is_distinct_from(b)))
+        self.assertFalse(self._select_bool(literal(a).isnot_distinct_from(b)))
 
-        values = [None, (1, None), (None, None), (1, (1, None))]
-        for v in values:
-            with self.subTest():
-                self.assertTrue(self._select_bool(literal(v).isnot_distinct_from(v)))
-                self.assertFalse(self._select_bool(literal(v).is_distinct_from(v)))
+    @parameterized.expand([
+        (None, ),
+        ((1, None), ),
+        ((None, None), ),
+        ((1, (1, None)), )
+    ])
+    def test_is_self_distinct_from(self, v):
+        self.assertTrue(self._select_bool(literal(v).isnot_distinct_from(v)))
+        self.assertFalse(self._select_bool(literal(v).is_distinct_from(v)))
