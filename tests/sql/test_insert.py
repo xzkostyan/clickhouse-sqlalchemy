d0290c0c6a7e7b1ee9ac47961c79d72391f77292
kanthi Subramanian <subkanthi@gmail.com>
2023-10-04 15:55:44 -0400

    Added unit test for testing insert statements on Map data type.

--- tests/sql/test_insert.py
+++ tests/sql/test_insert.py
@@ -20,3 +20,21 @@ class InsertTestCase(NativeSessionTestCase):
 
             rv = self.session.execute(select(table.c.x)).scalar()
             self.assertEqual(rv, 'test')
+
+    @require_server_version(19, 3, 3)
+    def test_insert_map(self):
+        table = Table(
+            't', self.metadata(),
+            Column('x', types.Map(types.String, types.Int32), primary_key=True),
+            engines.Memory()
+        )
+
+        with self.create_table(table):
+            dict_map = dict(key1=1, Key2=2)
+            x = [
+                {'x': dict_map}
+            ]
+            self.session.execute(table.insert(), x)
+
+            rv = self.session.execute(select(table.c.x)).scalar()
+            self.assertEqual(rv, dict_map)
