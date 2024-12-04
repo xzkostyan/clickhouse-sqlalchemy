95916cb9e7b7963feb9dacbb9f0c4f4d0612cd32
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2024-12-04 11:14:58 -0600

    Test with Python 3.13 and add PyPI trove classifier

--- setup.py
+++ setup.py
@@ -71,13 +71,13 @@ setup(
 
         'Programming Language :: SQL',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
 
         'Topic :: Database',
         'Topic :: Software Development',
