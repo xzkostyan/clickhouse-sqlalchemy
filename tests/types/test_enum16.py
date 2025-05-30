e396f61484b98b550c29960de8ae865064657b05
Stanley Kudrow <stankudrow@yandex.ru>
2025-05-30 11:06:15 +0300

    ruff the enum test modules

--- tests/types/test_enum16.py
+++ tests/types/test_enum16.py
@@ -8,7 +8,7 @@ from tests.testcase import BaseTestCase, CompilationTestCase
 from tests.util import with_native_and_http_sessions
 
 
-class TestEnum(enum.IntEnum):
+class _TestEnum(enum.IntEnum):
     First = 1
     Second = 2
 
@@ -18,28 +18,28 @@ class Enum16CompilationTestCase(CompilationTestCase):
         table = Table(
             "test",
             BaseTestCase.metadata(),
-            Column("x", types.Enum16(TestEnum)),
+            Column("x", types.Enum16(_TestEnum)),
             engines.Memory(),
         )
 
         self.assertEqual(
             self.compile(CreateTable(table)),
             "CREATE TABLE test (x Enum16('First' = 1, 'Second' = 2)) "
-            "ENGINE = Memory"
+            "ENGINE = Memory",
         )
 
 
 @with_native_and_http_sessions
 class Enum8TestCase(BaseTestCase):
     table = Table(
-        'test', BaseTestCase.metadata(),
-        Column('x', types.Enum16(TestEnum)),
-        engines.Memory()
+        "test",
+        BaseTestCase.metadata(),
+        Column("x", types.Enum16(_TestEnum)),
+        engines.Memory(),
     )
 
     def test_select_insert(self):
-        value = TestEnum.First
+        value = _TestEnum.First
         with self.create_table(self.table):
-            self.session.execute(self.table.insert(), [{'x': value}])
-            self.assertEqual(self.session.query(self.table.c.x).scalar(),
-                             value)
+            self.session.execute(self.table.insert(), [{"x": value}])
+            self.assertEqual(self.session.query(self.table.c.x).scalar(), value)
