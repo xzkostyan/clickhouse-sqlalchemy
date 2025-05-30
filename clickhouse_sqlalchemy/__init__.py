d2ab5e7cb983cb41871c473ef2ab2bc431b8f561
Stanley Kudrow <stankudrow@yandex.ru>
2025-05-30 11:05:37 +0300

    purge the __version__ module attribute

--- clickhouse_sqlalchemy/__init__.py
+++ clickhouse_sqlalchemy/__init__.py
@@ -3,8 +3,6 @@ from .ext.declarative import get_declarative_base
 from .orm.session import make_session
 from .sql import Table, MaterializedView, select
 
-__version__ = "0.3.2"
-
 __all__ = (
     "MaterializedView",
     "Table",
