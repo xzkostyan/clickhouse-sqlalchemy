e7f29b7a4bb7edfe1264d13b9daa053f442db773
kostya <kostyan.lebedev@gmail.com>
2025-11-25 00:26:41 +0300

    Try python 3.13

--- setup.py
+++ setup.py
@@ -71,13 +71,12 @@ setup(
 
         'Programming Language :: SQL',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
 
         'Topic :: Database',
         'Topic :: Software Development',
@@ -94,7 +93,7 @@ setup(
         'Changes': github_url + '/blob/master/CHANGELOG.md'
     },
     packages=find_packages('.', exclude=["tests*"]),
-    python_requires='>=3.7, <4',
+    python_requires='>=3.9, <4',
     install_requires=[
         'sqlalchemy>=2.0.0,<2.1.0',
         'requests',
